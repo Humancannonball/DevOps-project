6e9fa6cba1a893f5ead2c8fb039514864e29dc89
Mark Mikula <Mark20.Mikula05@gmail.com>
2025-02-02 13:45:57 +0200

    Fix naming conventions in configuration files
    Update naming conventions in `azure-aks-terraform` files to follow standard conventions.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/Humancannonball/DevOps-project?shareId=XXXX-XXXX-XXXX-XXXX).

--- azure-aks-terraform/main.tf
+++ azure-aks-terraform/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_resource_group" "rg" {
-  for_each = { for key in local.config.ResourceGroups : key.Name => key }
+  for_each = { for key in local.config.resource_groups : key.Name => key }
 
   name     = each.value.Name
-  location = local.config.Location
+  location = local.config.location
   tags     = lookup(each.value, "Tags", null) != null ? each.value.Tags : {}
   lifecycle {
     prevent_destroy = true
@@ -10,10 +10,10 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = local.config.VirtualNetwork.Name
-  location            = local.config.Location
-  resource_group_name = azurerm_resource_group.rg[local.config.VirtualNetwork.ResourceGroupName].name
-  address_space       = local.config.VirtualNetwork.AddressSpaces
+  name                = local.config.virtual_network.Name
+  location            = local.config.location
+  resource_group_name = azurerm_resource_group.rg[local.config.virtual_network.ResourceGroupName].name
+  address_space       = local.config.virtual_network.AddressSpaces
 
   lifecycle {
     ignore_changes = [
@@ -23,43 +23,43 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 module "network-security-group" {
-  for_each = { for key in local.config.NetworkSecurityGroups : key.Name => key }
+  for_each = { for key in local.config.network_security_groups : key.Name => key }
 
   source = "./modules/network-security-group"
 
   network_security_group = each.value
   resource_group_name    = azurerm_resource_group.rg[each.value.ResourceGroupName].name
-  location               = local.config.Location
+  location               = local.config.location
 }
 
 module "route-table" {
-  for_each = { for key in local.config.RouteTables : key.Name => key }
+  for_each = { for key in local.config.route_tables : key.Name => key }
 
   source = "./modules/route-table"
 
   route_table         = each.value
   resource_group_name = azurerm_resource_group.rg[each.value.ResourceGroupName].name
-  location            = local.config.Location
+  location            = local.config.location
 }
 
 module "vnet-subnet" {
-  for_each = { for key in local.config.VirtualNetwork.Subnets : key.Name => key }
+  for_each = { for key in local.config.virtual_network.Subnets : key.Name => key }
 
   source = "./modules/vnet-subnet"
 
   subnet = each.value
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name = azurerm_virtual_network.vnet.resource_group_name
-  location = local.config.Location
+  location = local.config.location
 
   nsg_id = module.network-security-group[each.value.NetworkSecurityGroup].nsg_resource_id
   rt_id = module.route-table[each.value.RouteTable].rt_resource_id
 }
 
 resource "azurerm_user_assigned_identity" "k8s_identity" {
-  name                = local.config.UserAssignedIdentity.Name
-  resource_group_name = azurerm_resource_group.rg[local.config.UserAssignedIdentity.ResourceGroupName].name
-  location            = local.config.Location
+  name                = local.config.user_assigned_identity.Name
+  resource_group_name = azurerm_resource_group.rg[local.config.user_assigned_identity.ResourceGroupName].name
+  location            = local.config.location
 }
 
 resource "azurerm_role_assignment" "route_table_permission" {
@@ -69,10 +69,10 @@ resource "azurerm_role_assignment" "route_table_permission" {
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
-  name                = local.config.AzureKubernetesCluster.Name
-  location            = local.config.AzureKubernetesCluster.Location
-  resource_group_name = azurerm_resource_group.rg[local.config.AzureKubernetesCluster.ResourceGroupName].name
-  dns_prefix          = local.config.AzureKubernetesCluster.DnsPrefix
+  name                = local.config.azure_kubernetes_cluster.Name
+  location            = local.config.azure_kubernetes_cluster.Location
+  resource_group_name = azurerm_resource_group.rg[local.config.azure_kubernetes_cluster.ResourceGroupName].name
+  dns_prefix          = local.config.azure_kubernetes_cluster.DnsPrefix
 
   identity {
     type         = "UserAssigned"
@@ -81,15 +81,15 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 
   default_node_pool {
     name           = "agentpool"
-    vm_size        = local.config.AzureKubernetesCluster.NodeSize
-    node_count     = local.config.AzureKubernetesCluster.NodeCount
-    vnet_subnet_id = module.vnet-subnet[local.config.AzureKubernetesCluster.SubnetName].subnet_id
+    vm_size        = local.config.azure_kubernetes_cluster.NodeSize
+    node_count     = local.config.azure_kubernetes_cluster.NodeCount
+    vnet_subnet_id = module.vnet-subnet[local.config.azure_kubernetes_cluster.SubnetName].subnet_id
   }
 
   network_profile {
-    network_plugin    = local.config.AzureKubernetesCluster.NetworkProfile.NetworkPlugin
-    load_balancer_sku = local.config.AzureKubernetesCluster.NetworkProfile.LoadBalancerSku
-    outbound_type     = local.config.AzureKubernetesCluster.NetworkProfile.OutboundType
+    network_plugin    = local.config.azure_kubernetes_cluster.NetworkProfile.NetworkPlugin
+    load_balancer_sku = local.config.azure_kubernetes_cluster.NetworkProfile.LoadBalancerSku
+    outbound_type     = local.config.azure_kubernetes_cluster.NetworkProfile.OutboundType
   }
 
   role_based_access_control {
@@ -104,28 +104,28 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     = local.config.StorageAccount.Name
-  resource_group_name      = azurerm_resource_group.rg[local.config.StorageAccount.ResourceGroupName].name
-  location                 = local.config.Location
-  account_tier             = local.config.StorageAccount.AccountTier
-  account_replication_type = local.config.StorageAccount.AccountReplicationType
+  name                     = local.config.storage_account.Name
+  resource_group_name      = azurerm_resource_group.rg[local.config.storage_account.ResourceGroupName].name
+  location                 = local.config.location
+  account_tier             = local.config.storage_account.AccountTier
+  account_replication_type = local.config.storage_account.AccountReplicationType
   lifecycle {
     prevent_destroy = true
   }  
 }
 resource "azurerm_storage_container" "tfstate" {
-  name                  = local.config.StorageContainer[0].Name
+  name                  = local.config.storage_container[0].Name
   storage_account_name  = azurerm_storage_account.sa.name
-  container_access_type = local.config.StorageContainer[0].ContainerAccessType
+  container_access_type = local.config.storage_container[0].ContainerAccessType
   lifecycle {
     prevent_destroy = true
   }
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = local.config.ContainerRegistry.Name
-  resource_group_name = azurerm_resource_group.rg[local.config.ContainerRegistry.ResourceGroupName].name
-  location            = local.config.ContainerRegistry.Location
-  sku                 = local.config.ContainerRegistry.Sku
-  admin_enabled       = local.config.ContainerRegistry.AdminEnabled
+  name                = local.config.container_registry.Name
+  resource_group_name = azurerm_resource_group.rg[local.config.container_registry.ResourceGroupName].name
+  location            = local.config.container_registry.Location
+  sku                 = local.config.container_registry.Sku
+  admin_enabled       = local.config.container_registry.AdminEnabled
 }
